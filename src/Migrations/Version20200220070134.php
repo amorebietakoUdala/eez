2d231da06e6966d48c164d3c553b9c18fadbc375
Amorebietako Udala <informatika@amorebieta.eus>
2020-02-20 08:13:58 +0100

    - Set empty_data to 0 for heritage and housingExpediture on QuotaForm.

--- src/Migrations/Version20200220070134.php
+++ src/Migrations/Version20200220070134.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191126075053 extends AbstractMigration
+final class Version20200220070134 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,7 @@ final class Version20191126075053 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE expedient ADD modified_by_id INT DEFAULT NULL, DROP modified_by');
-        $this->addSql('ALTER TABLE expedient ADD CONSTRAINT FK_F38F876199049ECE FOREIGN KEY (modified_by_id) REFERENCES fos_user (id)');
-        $this->addSql('CREATE INDEX IDX_F38F876199049ECE ON expedient (modified_by_id)');
+        $this->addSql('ALTER TABLE quota CHANGE heritage heritage NUMERIC(12, 2) DEFAULT \'0\' NOT NULL, CHANGE total_household_income total_household_income NUMERIC(12, 2) DEFAULT \'0\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +30,6 @@ final class Version20191126075053 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE expedient DROP FOREIGN KEY FK_F38F876199049ECE');
-        $this->addSql('DROP INDEX IDX_F38F876199049ECE ON expedient');
-        $this->addSql('ALTER TABLE expedient ADD modified_by VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP modified_by_id');
+        $this->addSql('ALTER TABLE quota CHANGE heritage heritage NUMERIC(12, 2) NOT NULL, CHANGE total_household_income total_household_income NUMERIC(12, 2) NOT NULL');
     }
 }
