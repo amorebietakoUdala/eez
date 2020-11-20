674bd191f00ab83786e2d6da8b18244213fe484e
Informatika <informatika@amorebieta.eus>
2020-11-20 08:39:29 +0100

    Fixed migration script

--- migrations/Version20201119111710.php
+++ migrations/Version20201119111710.php
@@ -22,9 +22,11 @@ final class Version20201119111710 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, activated TINYINT(1) DEFAULT \'1\' NOT NULL, last_login DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('INSERT INTO user SELECT id, username, "[]", password, name as first_name, email, enabled as activated, last_login FROM eez.fos_user');
-        $this->addSql('DROP TABLE fos_user');
+        $this->addSql('ALTER TABLE quota DROP FOREIGN KEY FK_6C1C0FEDB03A8386');
+        $this->addSql('ALTER TABLE quota DROP FOREIGN KEY FK_6C1C0FED99049ECE');
         $this->addSql('ALTER TABLE quota ADD CONSTRAINT FK_6C1C0FEDB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE quota ADD CONSTRAINT FK_6C1C0FED99049ECE FOREIGN KEY (modified_by_id) REFERENCES user (id)');
+        $this->addSql('DROP TABLE fos_user');
     }
 
     public function down(Schema $schema): void
