ce334b340c262b5624ff76e20934bb4fa98a8d0b
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2021-09-13 13:55:29 +0200

    Fixed code after, dependency updates

--- assets/js/quota/list_server.js
+++ assets/js/quota/list_server.js
@@ -2,7 +2,7 @@ import '../../scss/quota/list.scss';
 
 import $ from 'jquery';
 import 'bootstrap-table';
-import 'tableexport.jquery.plugin/tableExport';
+import 'tableexport.jquery.plugin';
 import 'bootstrap-table/dist/extensions/export/bootstrap-table-export'
 import 'bootstrap-table/dist/locale/bootstrap-table-es-ES';
 import 'bootstrap-table/dist/locale/bootstrap-table-eu-EU';
