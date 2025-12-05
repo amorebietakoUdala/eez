63d3af3d40b77abd9f784e2a14889e631ea75314
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2025-12-05 08:52:02 +0100

    Updated symfony due to vulnerabilities. Removed FOSJSRouting.

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Qipsius\TCPDFBundle\QipsiusTCPDFBundle::class => ['all' => true],
-    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     AMREU\UserBundle\UserBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
